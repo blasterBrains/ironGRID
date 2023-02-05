a4da29475bc9fcf474570e5ef48debf6fe720715
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2023-02-05 17:57:18 -0600

    deleted spinners.ts

--- common/theme/spinners.ts
+++
@@ -1,9 +0,0 @@
-// need to finish spinner theming
-
-import { defineStyleConfig } from '@chakra-ui/react';
-export const spinnerTheme = defineStyleConfig({
-  defaultProps: {
-    size: 'lg',
-    variant: 'bold',
-  },
-});
